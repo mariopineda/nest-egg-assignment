8a2b223e582ce07948612270f652540d29e75af7
Clyde-Chinsworth-CS <a.yang2@share.epsb.ca>
2018-04-14 15:59:28 +0000

    Added comments

--- NestEgg.java
+++ NestEgg.java
@@ -9,23 +9,23 @@ public class NestEgg
 		Scanner kbReader = new Scanner(System.in);
 	
 		System.out.println("Nest Egg Calculator");
-		System.out.print("Enter your salary: $");
+		System.out.print("Enter your salary: $"); //takes user input for salary, later takes percentage
 		double salary = kbReader.nextDouble();
-		System.out.print("Enter the percentage of funds to put into the retirement fund: ");
+		System.out.print("Enter the percentage of funds to put into the retirement fund: "); //every year this amount is added to account
 		double save = kbReader.nextDouble();
-		if (save > 100 || save < 0)
+		if (save > 100 || save < 0) //runs if user chooses to put more than 100% of income or less than 0%
 		{
-			System.out.print("Please enter a sensible value. ");
+			System.out.print("Please enter a sensible value. "); 
 			save = kbReader.nextDouble();
 		}
-		System.out.print("Enter the annual growth percentage: ");
+		System.out.print("Enter the annual growth percentage: "); //account grows by this % every year
 		double growthRate = kbReader.nextDouble();
-		if (growthRate > 100 || growthRate < 0)
+		if (growthRate > 100 || growthRate < 0) //runs if user chooses the growth rate to be more than 100% or less than 0%
 		{
 			System.out.print("Please enter a sensible value. ");
 			growthRate = kbReader.nextDouble();
 		}
-		System.out.print("Enter the amount of years you want to save for: ");
+		System.out.print("Enter the amount of years you want to save for: "); //user inputs amount of years, calculates savings for that amount of years
 		int years = kbReader.nextInt(); 
 		double F = salary * save * 0.01; //base funds
 		
