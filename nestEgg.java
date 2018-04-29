7871aaf71cb7c3473fbb7224f73e8983d86ba33e
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 14:15:02 -0600

    Added print statement for when default values are used

--- nestEgg.java
+++ nestEgg.java
@@ -3,22 +3,26 @@ import java.util.*;
 public class nestEgg {
    public static void main(String args[]) {
       Scanner kbReader = new Scanner(System.in);
+      String defVal = "   Your input is not sensible or invalid. A default value has been assigned.";
       double salary, save, growth, inflation, fund = 0;
       int years;
       System.out.print("How much is your salary? ");
       salary = kbReader.nextDouble();
       if (salary <= 0) {
          salary = 51000;
+         System.out.println(defVal);
       }
       System.out.print("What percentage of your salary are you saving? ");
       save = kbReader.nextDouble();
       if (save <= 0 || save >= 100) {
          save = 15;
+         System.out.println(defVal);
       }
       System.out.print("What is the growth percentage of the retirement fund? ");
       growth = kbReader.nextDouble();
       if (growth <= 0 || growth >= 100) {
          growth = 3;
+         System.out.println(defVal);
       }
       System.out.print("Do you want your salary to grow with inflation? (Y/N) ");
       char choice = kbReader.next(".").charAt(0);
@@ -34,6 +38,7 @@ public class nestEgg {
       years = kbReader.nextInt();
       if (years <= 0) {
          years = 35;
+         System.out.println(defVal);
       }
       for (int i = 0; i < years; i++) {
          fund = fund * (1 + 0.01 * growth) + salary *save * 0.01; //Calculation for determining retirement fund
