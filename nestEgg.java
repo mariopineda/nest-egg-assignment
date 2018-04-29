61881a4314bd874f40f7e7705ea21f70c3ef4d51
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 13:40:10 -0600

    Created choice for inflation

--- nestEgg.java
+++ nestEgg.java
@@ -3,13 +3,21 @@ import java.util.*;
 public class nestEgg {
    public static void main(String args[]) {
       Scanner kbReader = new Scanner(System.in);
-      double salary, save, growth, fund = 0;
+      double salary, save, growth, inflation, fund = 0;
       System.out.print("How much is your salary? ");
       salary = kbReader.nextDouble();
       System.out.print("What percentage of your salary are you saving? ");
       save = kbReader.nextDouble();
       System.out.print("What is the growth percentage of the retirement fund? ");
       growth = kbReader.nextDouble();
+      System.out.print("Do you want your salary to grow with inflation? (Y/N) ");
+      char choice = kbReader.next(".").charAt(0);
+      if (choice == 'Y' || choice == 'y') {
+         
+      }
+      else {
+         
+      }
       for (int i = 0; i < 10; i++) {
          fund = fund * (1 + 0.01 * growth) + salary *save * 0.01; //Calculation for determining retirement fund
       }
