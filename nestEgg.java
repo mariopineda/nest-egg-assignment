d237d1e1dc041383574a9fcef285022079b8ec3c
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 13:24:24 -0600

    Created necessary variables

--- nestEgg.java
+++ nestEgg.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 public class nestEgg {
    public static void main(String args[]) {
-      
+      Scanner kbReader = new Scanner(System.in);
+      double salary, save, growth, fund = 0;
    }
 }
