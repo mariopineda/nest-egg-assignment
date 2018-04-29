cda0eb10348bb9337feba8a14bce84058d20bc1c
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 14:09:45 -0600

    Added sanity checks on user input

--- nestEgg.java
+++ nestEgg.java
@@ -7,10 +7,19 @@ public class nestEgg {
       int years;
       System.out.print("How much is your salary? ");
       salary = kbReader.nextDouble();
+      if (salary <= 0) {
+         
+      }
       System.out.print("What percentage of your salary are you saving? ");
       save = kbReader.nextDouble();
+      if (save <= 0 || save >= 100) {
+         
+      }
       System.out.print("What is the growth percentage of the retirement fund? ");
       growth = kbReader.nextDouble();
+      if (growth <= 0 || growth >= 100) {
+         
+      }
       System.out.print("Do you want your salary to grow with inflation? (Y/N) ");
       char choice = kbReader.next(".").charAt(0);
       if (choice == 'Y' || choice == 'y') {
@@ -23,6 +32,9 @@ public class nestEgg {
       }
       System.out.print("How long until retirement? ");
       years = kbReader.nextInt();
+      if (years <= 0) {
+         
+      }
       for (int i = 0; i < years; i++) {
          fund = fund * (1 + 0.01 * growth) + salary *save * 0.01; //Calculation for determining retirement fund
          salary = salary * (1 + inflation);
