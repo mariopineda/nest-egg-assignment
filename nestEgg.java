76dc3da5e6f219d3285f6e60cf3bc309e1ffcc1b
Mostafa Rizk <m.rizk.student@gmail.com>
2018-04-29 13:28:18 -0600

    Initialized variables

--- nestEgg.java
+++ nestEgg.java
@@ -4,5 +4,11 @@ public class nestEgg {
    public static void main(String args[]) {
       Scanner kbReader = new Scanner(System.in);
       double salary, save, growth, fund = 0;
+      System.out.print("How much is your salary? ");
+      salary = kbReader.nextDouble();
+      System.out.print("What percentage of your salary are you saving? ");
+      save = kbReader.nextDouble();
+      System.out.print("What is the growth percentage of the retirement fund? ");
+      growth = kbReader.nextDouble();
    }
 }
